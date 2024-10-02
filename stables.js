3eb434419e3f5476e1c10750af1e24d5189b7d84
Вика Монакова <vikamonakova@MacBook-Pro-Vika.local>
2024-10-02 13:59:43 -0700

    lab 4 code, 3 month discound

--- stables.js
+++ stables.js
@@ -1,6 +1,29 @@
+//leson 3
 let horseName = "HelloWorld";
 console.log(horseName);
-horseName = "Trump"
+horseName = "noone";
 console.log(horseName);
-let horseAge = "78"
-console.log(horseAge)
+let horseAge = "1";
+console.log(horseAge);
+let isHorseInside = true;
+console.log(isHorseInside);
+//leson 4
+console.log(typeof "6554");
+console.log(typeof true);
+const STABLE_MONTHLY_FEE = 100; //uppercase is preferable 
+console.log(STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE * 3);
+console.log(STABLE_MONTHLY_FEE - STABLE_MONTHLY_FEE);
+console.log("hi" + "buy" + "    me" + "burger");
+console.log("hi" + 2); //type coercion
+console.log("2" + 2); //type coercion
+console.log("2" - 2); //type coercion
+console.log(true - 2);
+console.log("hi" + false);
+console.log(`my horse does not like his name, ${horseName} because he is too young, only${horseAge} years old`);//concatenation
+// Lab4
+// new pull request after pushing 
+horseName = "Natasha";
+let horseNickname = "Nataly";
+console.log(`My Horse has a name ${horseName} but everyone call her ${horseNickname}`);
+console.log("Only Nataly's fans can save $" + ((STABLE_MONTHLY_FEE * 3) * 0.1) + ", from 3 months $" + (STABLE_MONTHLY_FEE * 3) + " subscription.");
