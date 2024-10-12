47443e027dd8e7ff2d6612916964739bf3f24c83
monavi77 <vmonakova@my.bcit.ca>
2024-10-12 00:40:34 -0700

    Added new horses and used an if condtition

--- stables.js
+++ stables.js
@@ -27,10 +27,44 @@ horseName = "Natasha";
 let horseNickname = "Nataly";
 console.log(`My Horse has a name ${horseName} but everyone call her ${horseNickname}`);
 console.log("Only Nataly's fans can save $" + ((STABLE_MONTHLY_FEE * 3) * 0.1) + ", from 3 months $" + (STABLE_MONTHLY_FEE * 3) + " subscription.");
-
+// lab-5
 if (!isHorseInside) {
     console.log(horseName + ' is outside');
 }
 else {
     console.log(horseName + ' is inside');
+}
+
+//lesson 6
+//
+let horseNames = [horseName, "Strawberry", "Beans"];
+let horseAges = [horseAge, 2, 10];
+let horseLocations = [isHorseInside, false, true];
+let crazyArray = ["hello", [2, 3, 4]]
+console.log(horseNames);
+console.log(horseNames.length);
+//horseNames.push("Lettuce");
+console.log(`WELCOME TO MY STABLES! There are ${horseNames.length} horses staying here! Their names are: ${horseNames[1]}, ${horseNames[2]}`);
+console.log("WELCOME TO MY STABLES! There are " + horseNames.length + "  horses staying here! Their names are " + horseNames[0] + "," + horseNames[2] + horseNames[1] + "!");
+if (horseNames[0] === "Natasha") {
+    console.log("Hi Natasha!")
+}
+if (horseNames.length > 2) {
+    console.log("Bye Natasha!")
+}
+
+
+//lab 6
+console.log("All horses like to check the code, that is why " + horseNames[0] + " invites " + horseNames[1] + " and " + horseNames[2] + " to the stables, so there are " + horseNames.length + " horses now!");
+if (horseNames[2] === "Beans") {
+    console.log(horseNames[2] + " is inside")
+}
+if (horseAges[1] === 1) {
+    console.log(horseNames[1] + " also inside")
+}
+else {
+    console.log(horseNames[0] + " also inside")
+}
+if (horseLocations[1] === isHorseInside) {
+    console.log(horseNames[1] + " is outside")
 }
